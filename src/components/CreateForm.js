7fbf78ae298bdd081f3320cf1dfd421f48380e74
lisafeng <lisafenglisa@yahoo.com>
2023-01-30 23:24:30 -0500

    create a post, popup a form, submit a form

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import "./CreateForm.css";
 
 function CreateForm(props) {
@@ -11,19 +11,43 @@ function CreateForm(props) {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
 
+  const submitForm = () => {
+    axios.post("http://localhost:3000/api/v1/items", {
+      title: title,
+      category: category,
+      location: location,
+      description: description,
+      finder_name: name,
+      email: email,
+      phone: phone,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitle("");
+    setCategory("");
+    setLocation("");
+    setDescription("");
+    setName("");
+    setEmail("");
+    setPhone("");
+  };
+
   return props.trigger ? (
-    <div className="CreateForm">
+    <form onSubmit={handleSubmit} className="CreateForm">
       <div className="uploadPost">
         <h2>Report A Found Item</h2>
         <label>What item did you find?</label>
         <input
           type="text"
+          value={title}
           onChange={(e) => {
             setTitle(e.target.value);
           }}
         />
         <label>Category</label>
-        <select onChange={(e) => setCategory(e.target.value)}>
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
           <option>-- Please Select Category --</option>
           <option value="Animals/Pets">Animals/Pets</option>
           <option value="Bags,Baggage,Luggage">Bags,Baggage,Luggage</option>
@@ -46,12 +70,14 @@ function CreateForm(props) {
         <label>Where did you find it?</label>
         <input
           type="text"
+          value={location}
           onChange={(e) => {
             setLocation(e.target.value);
           }}
         />
         <label>Description</label>
         <textarea
+          value={description}
           onChange={(e) => {
             setDescription(e.target.value);
           }}
@@ -60,6 +86,7 @@ function CreateForm(props) {
         <label>Please enter your name</label>
         <input
           type="text"
+          value={name}
           onChange={(e) => {
             setName(e.target.value);
           }}
@@ -67,6 +94,7 @@ function CreateForm(props) {
         <label>Please enter your email</label>
         <input
           type="text"
+          value={email}
           placeholder="example@gmail.com"
           onChange={(e) => {
             setEmail(e.target.value);
@@ -75,18 +103,21 @@ function CreateForm(props) {
         <label>Please enter your phone number</label>
         <input
           type="text"
+          value={phone}
           placeholder="(703)888-8888"
           onChange={(e) => {
             setPhone(e.target.value);
           }}
         />
         <h3>Thanks for your help!</h3>
-        <button>Submit</button>
+        <button type="submit" onClick={submitForm}>
+          Submit
+        </button>
         <button className="close-btn" onClick={() => props.setTrigger(false)}>
           close
         </button>
       </div>
-    </div>
+    </form>
   ) : (
     ""
   );
