a2eb5a7328bb5eaa180e91dd2a514bd226fbf10a
lisafeng <lisafenglisa@yahoo.com>
2023-02-01 15:37:24 -0500

    did some layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,25 +13,33 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Lost & Found</h1>
-      <section>
-        <p>I Found Something!</p>
-        <button onClick={() => setButtonPopup(true)}>Create A Post</button>
-        <CreateForm
-          trigger={buttonPopup}
-          setTrigger={setButtonPopup}
-        ></CreateForm>
-      </section>
-      <section>
-        <p>I Lost Something!</p>
-        <button
-          onClick={() => {
-            setGoToBrowse(true);
-          }}
-        >
-          Start Browsing
-        </button>
-      </section>
+      {/* <h1>Lost & Found</h1> */}
+      <div className="title">
+        <img
+          src={require("./images/lost-and-found.jpg")}
+          alt="lost-and-found-icon"
+        />
+      </div>
+      <div className="wrapper">
+        <section className="found">
+          <p>I Found Something!</p>
+          <button onClick={() => setButtonPopup(true)}>Create A Post</button>
+          <CreateForm
+            trigger={buttonPopup}
+            setTrigger={setButtonPopup}
+          ></CreateForm>
+        </section>
+        <section className="lost">
+          <p>I Lost Something!</p>
+          <button
+            onClick={() => {
+              setGoToBrowse(true);
+            }}
+          >
+            Start Browsing
+          </button>
+        </section>
+      </div>
     </div>
   );
 };
